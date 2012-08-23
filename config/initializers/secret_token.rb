9eee3d83de1a0627362f6fc6cbc877084cf27c8e
Rohit Rox <link2j.roy@gmail.com>
2012-08-23 11:38:45 +0545

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsHangman::Application.config.secret_token = 'bcccab20a071040350ff96ae10f95377f69fe101682913bfd8b8f71d32bd80c388b78496ef924e662105059f416f63f9de987c7f958d79aa2dad958d41bbdc64'
