013fd797d1b87e8f73587b30513d4693efcd87b4
Rohit Rox <link2j.roy@gmail.com>
2012-08-24 13:56:16 +0545

    faye url fix for heroku

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
 });
 
 $(function() {
-  var faye = new Faye.Client('http://localhost:9292/faye');
+  var faye = new Faye.Client('http://hangman-app.herokuapp.com:9292/faye');
   faye.subscribe('/messages/new', function (data) {
     eval(data);
   });
