bc9a95ed56d693a24240a7accf4a7dffc4431dbe
Rohit Rox <link2j.roy@gmail.com>
2012-08-24 16:17:09 +0545

    faye url change according to my-faye-server url

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
 });
 
 $(function() {
-  var faye = new Faye.Client('http://hangman-app.herokuapp.com:9292/faye');
+  var faye = new Faye.Client('http://my-faye-server.herokuapp.com/faye');
   faye.subscribe('/messages/new', function (data) {
     eval(data);
   });
