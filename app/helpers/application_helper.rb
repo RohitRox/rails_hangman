6ed4cc645727aaed148df547c3dd0ca54db02c84
Rohit Rox <link2j.roy@gmail.com>
2012-08-24 16:25:59 +0545

    typo fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block)}
-    uri = URI.parse("http://my-faye-server.herokuapp.com/faye/faye")
+    uri = URI.parse("http://my-faye-server.herokuapp.com/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
